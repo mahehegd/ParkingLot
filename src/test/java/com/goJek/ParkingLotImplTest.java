4b6f51da30d447b485ce295cf5e1671622d8d978
Mahesh <mahesh@circles.asia>
2020-03-30 01:30:57 +0800

    Funcitonal Unit tests for ParkingLotImpl

--- src/test/java/com/goJek/ParkingLotImplTest.java
+++ src/test/java/com/goJek/ParkingLotImplTest.java
@@ -65,5 +65,70 @@ public class ParkingLotImplTest {
         assertEquals(expected,consoleOutput.toString() );
     }
 
+    @Test
+    public void TestRegistrationNumbersForCarsWithColour(){
+        ParkingLot parkingLot = new ParkingLotImpl();
+        commandManager.executeCommand("create_parking_lot 6", parkingLot);
+        commandManager.executeCommand("park ka-25-mb-0812 red", parkingLot);
+        commandManager.executeCommand("park ka-35-aq-0812 white", parkingLot);
+        commandManager.executeCommand("park ka-18-by-3784 red", parkingLot);
+
+        commandManager.executeCommand("registration_numbers_for_cars_with_colour red", parkingLot);
+        String expected = "Created a parking lot with 6 slots\n" +
+                "Allocated slot number: 1\n" +
+                "Allocated slot number: 2\n" +
+                "Allocated slot number: 3\n" +
+                "ka-25-mb-0812, ka-18-by-3784\n";
+        assertEquals(expected,consoleOutput.toString() );
+    }
+
+    @Test
+    public void TestSlotNumbersForCarsWithColours() {
+        ParkingLot parkingLot = new ParkingLotImpl();
+        commandManager.executeCommand("create_parking_lot 6", parkingLot);
+        commandManager.executeCommand("park ka-25-mb-0812 red", parkingLot);
+        commandManager.executeCommand("park ka-35-aq-0812 white", parkingLot);
+        commandManager.executeCommand("park ka-18-by-3784 red", parkingLot);
+
+        commandManager.executeCommand("slot_numbers_for_cars_with_colour red", parkingLot);
+        String expected = "Created a parking lot with 6 slots\n" +
+                "Allocated slot number: 1\n" +
+                "Allocated slot number: 2\n" +
+                "Allocated slot number: 3\n" +
+                "1, 3\n";
+        assertEquals(expected,consoleOutput.toString() );
+    }
+
+    @Test
+    public void TestSlotNumberForRegistrationNumber() {
+        ParkingLot parkingLot = new ParkingLotImpl();
+        commandManager.executeCommand("create_parking_lot 6", parkingLot);
+        commandManager.executeCommand("park ka-25-mb-0812 red", parkingLot);
+        commandManager.executeCommand("park ka-35-aq-0812 white", parkingLot);
+        commandManager.executeCommand("park ka-18-by-3784 red", parkingLot);
+
+        commandManager.executeCommand("slot_number_for_registration_number ka-35-aq-0812", parkingLot);
+
+        String expected = "Created a parking lot with 6 slots\n" +
+                "Allocated slot number: 1\n" +
+                "Allocated slot number: 2\n" +
+                "Allocated slot number: 3\n" +
+                "2\n";
+        assertEquals(expected,consoleOutput.toString() );
+    }
+
+    @Test
+    public void TestParkingLotFullMessage(){
+        ParkingLot parkingLot = new ParkingLotImpl();
+        commandManager.executeCommand("create_parking_lot 1", parkingLot);
+        commandManager.executeCommand("park ka-25-mb-0812 red", parkingLot);
+        commandManager.executeCommand("park ka-35-aq-0812 white", parkingLot);
+
+        String expected = "Created a parking lot with 1 slots\n" +
+                "Allocated slot number: 1\n" +
+                "Sorry, parking lot is full\n";
+        assertEquals(expected,consoleOutput.toString() );
+
+    }
 
 }
