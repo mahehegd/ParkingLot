b2fb47d9d868c487f2d7bc5f10671a398f820a37
Mahesh <mahesh@circles.asia>
2020-03-29 23:34:26 +0800

    Input reader for console and File

---
+++ src/main/java/com/goJek/inputReader/ConsoleReader.java
@@ -0,0 +1,15 @@
+package com.goJek.inputReader;
+
+import com.goJek.manager.CommandManager;
+
+public class ConsoleReader {
+    CommandManager commandManager;
+
+    public ConsoleReader() {
+        this.commandManager = new CommandManager();
+    }
+
+    public void readAndExecuteCommand() {
+
+    }
+}
---
+++ src/main/java/com/goJek/manager/FileManager.java
@@ -0,0 +1,23 @@
+package com.goJek.manager;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+public class FileManager extends CommandManager {
+
+    public void executeCommandFromFile(String file, ParkingLot park){
+        File commandFile = new File(file);
+        try{
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(commandFile));
+            String line = "";
+
+            while((line = bufferedReader.readLine())!= null) {
+                executeCommand(line , park);
+            }
+
+        }catch(Exception e){
+            System.out.println("Error handling file "+ e.getStackTrace());
+        }
+    }
+}
